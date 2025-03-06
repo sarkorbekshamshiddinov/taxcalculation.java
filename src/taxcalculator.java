63a2288c6dba95ff69c6180d7bbabb45049685d1
sarkorbekshamshiddinov <sarkorbekw05@gmail.com>
2025-03-06 16:05:42 +0100

    Kod yangilandi

--- src/taxcalculator.java
+++ src/taxcalculator.java
@@ -1,3 +1,6 @@
+import java.math.BigDecimal;
+
+
 public class taxcalculator {
         public static void main(String[] args) {
             double productPrice = 9.99;
@@ -12,7 +15,24 @@ public class taxcalculator {
             System.out.println("Using Double:");
             System.out.println("Gross Price: " + grossPrice);
             System.out.println("Total annual value for 10,000 units: " + totalannualValue);
-            System.out.println("Total Net Value excluding vat: " + totalNetValue);
+            System.out.println("Total net value :" +totalNetValue);
+            // With BigDecimal
+            BigDecimal netPrice = new BigDecimal("9.99");
+            BigDecimal VatRate = new BigDecimal("0.23");
+            BigDecimal NetgrossPrice = netPrice.multiply(BigDecimal.ONE.add(VatRate));
+            BigDecimal annualgross = NetgrossPrice.multiply(new BigDecimal("10000"));
+            BigDecimal annualNetValue = annualgross.divide(BigDecimal.ONE.add(VatRate));
+
+            System.out.println("\nUsing BigDecimal:");
+            System.out.println("Gross Price: " + NetgrossPrice);
+            System.out.println("Total Gross (10,000 pcs): " + annualgross);
+            System.out.println("Total Net : " + annualNetValue);
+
+            //Comparison between Double and BgDecimal
+          System.out.println("\nComparison:");
+            System.out.println("Total net with Double : " +(totalannualValue));
+            System.out.println("Total Net with BigDecimal : " +(annualNetValue));
+
         }
     }
 
